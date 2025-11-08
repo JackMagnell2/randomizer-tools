d67329ec5fe1c7e2e9fc358122937e0d27889716
JackMagnell2 <ja1035ma-s@student.lu.se>
2025-11-08 16:38:54 +0100

    Initial Blazor project with folder structure

---
+++ Services/RandomizerService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Random;
+using System.Security.Cryptography.RandomNumberGenerator;
+
+namespace RandomizerTools.Services
+{
+    public class RandomizerService
+    {
+        
+    }
+}
