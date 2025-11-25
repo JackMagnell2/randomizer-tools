012eba8548c8d82e31f851d445a49fb47a118e4e
JackMagnell2 <ja1035ma-s@student.lu.se>
2025-11-25 17:13:29 +0100

    Wheel of names viewmodel completed

--- ViewModels/WheelOfNamesViewModel.cs
+++ ViewModels/WheelOfNamesViewModel.cs
@@ -14,10 +14,52 @@ namespace RandomizerTools.ViewModels
 
         public List<WheelEntry> Entries { get; set; }
 
+        public WheelEntry? SelectedWinner { get; set;}
+
         public WheelOfNamesViewModel(RandomizerService randomizer)
         {
             _randomizer = randomizer;
             Entries = new List<WheelEntry>();
+        }
+
+        /// <summary>
+        /// Adds an entry to the wheel
+        /// </summary>
+        /// <param name="name">The name of the entry</param>
+        public void AddEntry(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return;
+
+            var entry = new WheelEntry(name);
+
+            Entries.Add(entry);
+        }
+
+        /// <summary>
+        /// Removes an entry from the wheel
+        /// </summary>
+        /// <param name="entry">The entry to be removed</param>
+        public void RemoveEntry(WheelEntry entry)
+        {
+            if (entry == null)
+                return;
+            
+            Entries.Remove(entry);
+        }
+
+        /// <summary>
+        /// Spins the wheel and selects a random winner
+        /// </summary>
+        public void SpinWheel()
+        {
+            if (Entries.Count == 0)
+            {
+                SelectedWinner = null;
+                return;
+            }
+
+            SelectedWinner = _randomizer.GetRandomItem(Entries);
         }
     }
 }
